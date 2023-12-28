460935039b46b73f50f5ec094642a68541a7bff5
Ismail <shaikhnagismail@gmail.com>
2023-12-28 22:23:09 +0200

    feat: Add Google Maps link to location in HeaderInformation component

--- components/HeaderInformation.tsx
+++ components/HeaderInformation.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Link from "next/link";
+
 import { MapPin, Phone, Clock } from "lucide-react";
+
+const googleMapsLink =
+  "https://www.google.com/maps/place/Auto+Wheels+%26+Exhausts/@-25.8632276,25.6459493,17z/data=!3m1!4b1!4m6!3m5!1s0x1ea2cac697680bb7:0x2ad30578d491e15a!8m2!3d-25.8632276!4d25.6459493!16s%2Fg%2F1pzr6fdt5?entry=ttu";
 
 const HeaderInformation: React.FC = () => {
   return (
@@ -31,12 +36,19 @@ const HeaderInformation: React.FC = () => {
 
         {/* location (seen after md)*/}
         <address
-          className="hidden md:flex gap-x-2 items-center text-sm "
+          className="hidden md:flex text-sm "
           role="contentinfo"
           aria-label="Location"
         >
-          <MapPin className="h-4 w-4" aria-hidden />
-          <p>23 Nelson Mandela Dr, Mahikeng</p>
+          <Link
+            href={googleMapsLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-x-2 items-center"
+          >
+            <MapPin className="h-4 w-4" aria-hidden />
+            <p>23 Nelson Mandela Dr, Mahikeng</p>
+          </Link>
         </address>
       </div>
     </header>
--- components/navbar/DesktopMenu.tsx
+++ components/navbar/DesktopMenu.tsx
@@ -4,8 +4,6 @@ import Link from "next/link";
 import { navLinks } from "@/constants";
 
 const DesktopMenu: React.FC = () => {
-  // Add your component logic here
-
   return (
     <ul className="text-white gap-x-8 hidden md:flex text-sm lg:text-base">
       {navLinks.map((link) => (
